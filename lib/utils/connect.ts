f24622b57f2081bb2d4ac4c78730a03918e3aa87
liuxinwu <1525287364@qq.com>
2021-11-23 17:42:29 +0800

    处理重连逻辑

--- lib/utils/connect.ts
+++ lib/utils/connect.ts
@@ -9,14 +9,23 @@ export const handleConnect = <T>(
   instance: ViteRequest,
   config: AxiosRequestConfig,
   _customConfig: CustomConfigType,
-  requestKey: string
+  requestKey: string,
+  isDelete: boolean = false
 ) => {
+  if (isDelete) {
+    connectMap.delete(requestKey)
+    return
+  }
+
   // 处理重连
   if (!connectMap.has(requestKey)) {
     connectMap.set(requestKey, 1)
   }
-  let connectCount = connectMap.get(requestKey) + 1
-  if (connectCount < _customConfig.connectCount) {
+
+  let connectCount = connectMap.get(requestKey)
+
+  if (connectCount <= _customConfig.connectCount) {
+    connectCount += 1
     connectMap.set(requestKey, connectCount)
     // @ts-ignore
     return instance.request<T>(config, _customConfig)
